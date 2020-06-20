12c3aa454d966a0fa5a88224090e90675bfa9726
erik <errik25@gmail.com>
2020-06-20 23:16:47 +0500

    Updates

---
+++ precache-manifest.48540b886b2258a65de5c5af8861b05e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9e57fe2a3e1899001ced2003caf026",
+    "url": "/vk_mini_app/index.html"
+  },
+  {
+    "revision": "01f167924900c5adf2de",
+    "url": "/vk_mini_app/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "01f167924900c5adf2de",
+    "url": "/vk_mini_app/static/js/2.cce0294b.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/vk_mini_app/static/js/2.cce0294b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd30354161b835334b5e",
+    "url": "/vk_mini_app/static/js/main.b4abb52f.chunk.js"
+  },
+  {
+    "revision": "9781d0a3e762fff6d5f9",
+    "url": "/vk_mini_app/static/js/runtime-main.159d045e.js"
+  }
+]);
