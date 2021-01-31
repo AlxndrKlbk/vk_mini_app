d650b945ef7d658d0999c7a19206012e8162fe0c
Alexander Kolbeko <sasha-kolbeko@mail.ru>
2021-01-31 20:07:38 +0500

    begin writing js server starter

---
+++ server_starter.py
@@ -0,0 +1,8 @@
+import os
+
+dir = os.path.dirname(__file__)
+file_path = f"{dir}/main.py"
+os.system(file_path)
+
+
+# можно дописать сюда интерфейс для прокидывания запросов через request, без привлечения postman/arc
